28377b69ce5ca2efa8d57e5c2b387a4b7ccaca12
Joris Van den Broek (ENU) <JO256167@wipro.com>
2017-08-14 15:47:00 +0200

    add GET /todos/:id rout

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ var bodyParser = require('body-parser');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+var {ObjectID} = require('mongodb');
 
 var app = express();
 
@@ -27,6 +28,22 @@ app.get('/todos', (req, res) => {
       res.status(400).send(err);
     })
 });
+
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  if (!ObjectID.isValid(id)){
+    res.status(404).send();
+  }
+//  res.send(req.params);
+  Todo.findById(req.params.id).then((todo) => {
+    if(!todo){
+      res.status(404).send();
+    }
+    res.send(todo)
+  }).catch((e) => {
+    res.status(400).send();
+  });
+})
 
 app.listen(3000, () => {
   console.log('App started on port 3000');
