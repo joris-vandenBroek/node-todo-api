d00d5a9a6cbd1edcbd88106c049f42bf1ff8a20d
Joris Van den Broek (ENU) <JO256167@wipro.com>
2017-08-16 15:08:13 +0200

    Setup app for Heroku

--- server/server.js
+++ server/server.js
@@ -6,6 +6,8 @@ var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 var {ObjectID} = require('mongodb');
 
+const port = process.env.PORT || 3000;
+
 var app = express();
 
 app.use(bodyParser.json());
@@ -45,8 +47,8 @@ app.get('/todos/:id', (req, res) => {
   });
 })
 
-app.listen(3000, () => {
-  console.log('App started on port 3000');
+app.listen(port, () => {
+  console.log(`App started on port ${port}`);
 });
 
 module.exports = {app};
