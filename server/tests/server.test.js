94954c9215b27b7afaf610cab2fef996b110ad3f
Joris Van den Broek (ENU) <JO256167@wipro.com>
2017-08-16 14:07:22 +0200

    Test cases for get todos by id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -1,12 +1,15 @@
 const expect = require('expect');
 const request = require('supertest');
+var {ObjectID} = require('mongodb');
 
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
 const todos = [{
+  _id: new ObjectID(),
   text: 'First test todo'
 }, {
+  _id: new ObjectID(),
   text: 'Second test todo'
 }];
 
@@ -19,8 +22,7 @@ beforeEach((done) => {
 describe('POST /todos', () => {
   it('should create a new todo', (done) => {
     var text = 'Test todo text';
-
-    request(app)
+  request(app)
     .post('/todos')
     .send({text})
     .expect(200)
@@ -38,6 +40,7 @@ describe('POST /todos', () => {
       }).catch((e) => {done(e)});
     });
   });
+
   it('should not create todo with invalid body data', (done) => {
     request(app)
     .post('/todos')
@@ -57,7 +60,6 @@ describe('POST /todos', () => {
 
 describe('GET /todos', () => {
   it('should get all todos', (done) => {
-
     request(app)
     .get('/todos')
     .expect(200)
@@ -65,5 +67,32 @@ describe('GET /todos', () => {
       expect(res.body.todos.length).toBe(2);
     })
     .end(done);
+  });
+});
+
+describe('GET /todos/:id', () => {
+  it('should return a todo doc by id', (done) => {
+    request(app)
+    .get(`/todos/${todos[0]._id.toHexString()}`)
+    .expect(200)
+    // .expect((res) => {
+    //   expect(res.body.todo.text).toBe(todos[0].text);
+    // })
+    .end(done);
+  });
+
+  it('should return 404 when todo not found using valid id', (done) => {
+      var id= new ObjectID().toHexString();
+      request(app)
+      .get(`/todos/${id}`)
+      .expect(404)
+      .end(done);
+  });
+
+  it('should return 404 when todo not found using invalid id', (done) => {
+      request(app)
+      .get(`/todos/123`)
+      .expect(404)
+      .end(done);
   });
 });
